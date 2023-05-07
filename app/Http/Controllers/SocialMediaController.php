c833216eed569da98f167b1bca6137ad0495e322
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2023-05-06 21:24:05 -0500

    crear endpoint para social media

---
+++ app/Http/Controllers/SocialMediaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SocialMedia;
+use Illuminate\Http\Request;
+
+class SocialMediaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(SocialMedia::get());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SocialMediaController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::controller(UserController::class)->group(function () {
     Route::get('/', 'index');
+});
+
+Route::controller(SocialMediaController::class)->group(function () {
+    Route::get('social-media', 'index');
 });
