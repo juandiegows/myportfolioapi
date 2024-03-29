4d63f2f417411004b71a5959a56d97620d4a71e3
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-03-28 22:48:44 -0500

    remover librerias

--- app/Http/Controllers/IpAddressController.php
+++ app/Http/Controllers/IpAddressController.php
@@ -4,23 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Torann\GeoIP\Facades\GeoIP;
 
 class IpAddressController extends Controller
 {
     public function getIpAddress(Request $request)
     {
-        $ipv4 = $request->ip();
+        $response = Http::get('https://api.ipify.org/?format=json');
+
+        if ($response->successful()) {
+            $data = $response->json();
+            $ipv4 = $data['ip'];
+        } else {
+            $ipv4 = $request->ip();
+        }
+
         $ipv6 = $request->getClientIp();
         $client = $request->userAgent() ?: '';
         $https = $request->secure();
-        $location = GeoIP::getLocation($ipv6);
-        $ipv4 = inet_ntop(substr(@inet_pton($ipv6), 12));
 
         return response()->json([
             'ipv4' => $ipv4,
             'ipv6' => $ipv6,
-            'location' =>  $location,
             'client' => $client,
             'useHttps' => $https
         ]);
