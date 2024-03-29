88dbae5923dc2cfef7eafd16531461992c155ec7
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-03-28 22:37:51 -0500

    incluir localizacion

--- app/Http/Controllers/IpAddressController.php
+++ app/Http/Controllers/IpAddressController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Torann\GeoIP\Facades\GeoIP;
 
 class IpAddressController extends Controller
 {
@@ -13,15 +14,13 @@ class IpAddressController extends Controller
         $ipv6 = $request->getClientIp();
         $client = $request->userAgent() ?: '';
         $https = $request->secure();
+        $location = GeoIP::getLocation($ipv6);
+        $ipv4 = inet_ntop(substr(@inet_pton($ipv6), 12));
 
-        $binary = @inet_pton($ipv6);
-
-        if ($binary) {
-            $ipv4 = @inet_ntop(substr($binary, 12));
-        }
         return response()->json([
             'ipv4' => $ipv4,
             'ipv6' => $ipv6,
+            'location' =>  $location,
             'client' => $client,
             'useHttps' => $https
         ]);
