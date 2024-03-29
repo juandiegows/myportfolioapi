478c0837c9153e48d19fdcd3e026fc71d2b38a14
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-03-28 22:20:51 -0500

    ip puebra dos

--- app/Http/Controllers/IpAddressController.php
+++ app/Http/Controllers/IpAddressController.php
@@ -9,13 +9,21 @@ class IpAddressController extends Controller
 {
     public function getIpAddress(Request $request)
     {
-        $ipv4 = $request->ips();
-        $ipv6 = $request->getClientIps();
-        $client = $request->userAgent() ?:'';
+        $ipv4 = $request->ip();
+        $ipv6 = $request->getClientIp();
+        $ipv62 = $request->header('X-Real-IP');
+        if (!$ipv62) {
+            $ipv62 = $request->header('X-Forwarded-For');
+        }
+        if (!$ipv62) {
+            $ipv62 = $request->header('Forwarded');
+        }
+        $client = $request->userAgent() ?: '';
         $https = $request->secure();
         return response()->json([
             'ipv4' => $ipv4,
             'ipv6' => $ipv6,
+            'ipv62' => $ipv62,
             'client' => $client,
             'useHttps' => $https
         ]);
