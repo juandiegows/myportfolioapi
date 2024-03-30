b85488d26171d729aa5b3ddde234c6fe5b1dfc71
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-03-30 11:21:59 -0500

    mostrar el navegador desde el que te conectas

--- app/Http/Controllers/IpAddressController.php
+++ app/Http/Controllers/IpAddressController.php
@@ -20,8 +20,29 @@ class IpAddressController extends Controller
         return response()->json([
             'ipv4' => $ipv4,
             'ipv6' => $ipv6,
-            'client' => $client,
+            'browser' => $this->getBrowser($client),
             'useHttps' => $https
         ]);
+    }
+
+    function getBrowser($client)
+    {
+
+        if (strpos($client, 'MSIE') !== false)
+            return 'Internet explorer';
+        elseif (strpos($client, 'Trident') !== false)
+            return 'Internet explorer';
+        elseif (strpos($client, 'Firefox') !== false)
+            return 'Mozilla Firefox';
+        elseif (strpos($client, 'Chrome') !== false)
+            return 'Google Chrome';
+        elseif (strpos($client, 'Opera Mini') !== false)
+            return "Opera Mini";
+        elseif (strpos($client, 'Opera') !== false)
+            return "Opera";
+        elseif (strpos($client, 'Safari') !== false)
+            return "Safari";
+        else
+            return 'Other';
     }
 }
