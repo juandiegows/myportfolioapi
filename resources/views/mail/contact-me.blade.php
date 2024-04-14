2e5c2f49067ec05627e1d60832fd868dd8def3f8
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-04-14 13:09:51 -0500

    prueba 003

--- resources/views/mail/contact-me.blade.php
+++ resources/views/mail/contact-me.blade.php
@@ -4,13 +4,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://api.juandiegows.com/css/style.email.css">
 </head>
-<div class="content">
-    <main class="main">
-        <p style="background: yellow">hola, Mr. Juan Diego</p>
-        <p> {{ $name ?? "Juan Diego Mejia Maestre"  }} quiere ponerse en contacto contigo.</p>
-        <p> <b class="titulo"> asunto :</b> {{ $subject ?? 'prueba de correo'  }}</p>
-        <p> <b class="titulo"> Email :</b> {{ $email ?? 'jdmeji@misena.edu.co' }}</p>
-        <p> <b class="titulo"> Mensaje : </b> {{ $messageText ?? 'este es el mensaje del correo' }}</p>
-        <p> <b class="titulo"> Fecha :</b> {{ $created_at ?? now() }}</p>
+<div class="content" style="box-sizing: border-box; background: #92a8d1; width: 100%; height: 100%; padding: 0% 20%; display: grid; place-content: center; font-family: 'Montserrat', sans-serif; font-optical-sizing: auto; font-style: normal;">
+    <main class="main" style="width: 100%; min-width: 50%; background-color: #0150bc; padding: 5%; border-radius: 10px;">
+        <p style="margin: 5px 0; color: white; background: yellow;">hola, Mr. Juan Diego</p>
+        <p style="margin: 5px 0; color: white;">{{ $name ?? "Juan Diego Mejia Maestre" }} quiere ponerse en contacto contigo.</p>
+        <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> asunto :</b> {{ $subject ?? 'prueba de correo' }}</p>
+        <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> Email :</b> {{ $email ?? 'jdmeji@misena.edu.co' }}</p>
+        <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> Mensaje : </b> {{ $messageText ?? 'este es el mensaje del correo' }}</p>
+        <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> Fecha :</b> {{ $created_at ?? now() }}</p>
     </main>
 </div>
