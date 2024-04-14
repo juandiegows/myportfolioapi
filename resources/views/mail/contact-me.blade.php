ec8b95ac08f1d81d7c141c6e59450f6a5d95d37d
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-04-14 16:13:43 -0500

    cambiar fuente

--- resources/views/mail/contact-me.blade.php
+++ resources/views/mail/contact-me.blade.php
@@ -11,6 +11,7 @@
 
        <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
            html,
            body,
            div,
