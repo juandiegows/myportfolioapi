216f4943d3212f971b23b7f64be079e6039a9025
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-04-14 13:02:52 -0500

    ponerle estilo prueba dos

--- resources/views/mail/contact-me.blade.php
+++ resources/views/mail/contact-me.blade.php
@@ -1,7 +1,12 @@
-<link rel="stylesheet" href="https://api.juandiegows.com/css/style.email.css">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://api.juandiegows.com/css/style.email.css">
+</head>
 <div class="content">
     <main class="main">
-        <p>hola, Mr. Juan Diego</p>
+        <p style="background: yellow">hola, Mr. Juan Diego</p>
         <p> {{ $name ?? "Juan Diego Mejia Maestre"  }} quiere ponerse en contacto contigo.</p>
         <p> <b class="titulo"> asunto :</b> {{ $subject ?? 'prueba de correo'  }}</p>
         <p> <b class="titulo"> Email :</b> {{ $email ?? 'jdmeji@misena.edu.co' }}</p>
