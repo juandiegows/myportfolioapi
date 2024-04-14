3f8d9e68d8127631ac1157bb30bf832a1e39cf88
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-04-14 13:21:17 -0500

    quitar fondo amarrillo

--- resources/views/mail/contact-me.blade.php
+++ resources/views/mail/contact-me.blade.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" href="https://api.juandiegows.com/css/style.email.css">
 </head>
 <div class="content" style="box-sizing: border-box; background: #92a8d1; width: 100%; height: 100%; padding: 0% 20%; display: grid; place-content: center; font-family: 'Montserrat', sans-serif; font-optical-sizing: auto; font-style: normal;">
-    <main class="main" style="width: 100%; min-width: 50%; background-color: #0150bc; padding: 5%; border-radius: 10px;">
-        <p style="margin: 5px 0; color: white; background: yellow;">hola, Mr. Juan Diego</p>
+    <main class="main" style="width: 100%; min-width: 50%; background-color: #0150bc; padding: 5%; border-radius: 10px; hegth:100vh;">
+        <p style="margin: 5px 0; color: white;">hola, Mr. Juan Diego</p>
         <p style="margin: 5px 0; color: white;">{{ $name ?? "Juan Diego Mejia Maestre" }} quiere ponerse en contacto contigo.</p>
         <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> asunto :</b> {{ $subject ?? 'prueba de correo' }}</p>
         <p style="margin: 5px 0; color: white;"><b style="text-transform: capitalize; font-weight: 700; color: #fc804a;"> Email :</b> {{ $email ?? 'jdmeji@misena.edu.co' }}</p>
