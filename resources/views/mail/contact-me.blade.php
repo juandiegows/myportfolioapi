792f9857806cc3564071e2e72e65e87e000012f4
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2024-04-14 14:47:20 -0500

    prueba

--- resources/views/mail/contact-me.blade.php
+++ resources/views/mail/contact-me.blade.php
@@ -177,14 +177,14 @@
    <body>
 
        <div class="content">
-           <main class="main">
+           <div class="main">
                <p>hola, Mr. Juan Diego</p>
                <p> {{ $name ?? "Juan Diego Mejia Maestre"  }} quiere ponerse en contacto contigo,</p>
                <p> <b class="titulo"> asunto :</b> {{ $subject ?? 'prueba de correo'  }}</p>
                <p> <b class="titulo"> Email :</b> {{ $email ?? 'jdmeji@misena.edu.co' }}</p>
                <p> <b class="titulo"> Mensaje : </b> {{ $messageText ?? 'este es el mensaje del correo' }}</p>
                <p> <b class="titulo"> Fecha :</b> {{ $created_at ?? now() }}</p>
-           </main>
+           </div>
 
 
        </div>
