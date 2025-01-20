dd52fed222900912f60f6893652f38f055006cfa
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-01-20 17:49:43 -0500

    actualizar work

--- database/seeders/WorkExperienceSeeder2.php
+++ database/seeders/WorkExperienceSeeder2.php
@@ -28,7 +28,7 @@ class WorkExperienceSeeder2 extends Seeder
                 'start_date' => '2023-06-01',
                 'end_date' => '2023-11-30',
             ]);
-            WorkExperience::find(7)
+            WorkExperience::find(6)
             ->update([
                 'start_date' => '2023-04-17',
                 'end_date' => '2023-09-17',
