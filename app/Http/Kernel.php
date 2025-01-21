7531a0a42a3007c99b2c7a2eb565b0bbe9e4a1ec
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-01-20 22:59:08 -0500

    AJUSTE cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,13 +36,14 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\HandleCors::class
+            \Illuminate\Http\Middleware\HandleCors::class,
         ],
     ];
 
