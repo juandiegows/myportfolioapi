b4a69fc553be470063bc6d69f8c5b7d6c07d8d77
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-01-20 22:42:18 -0500

    permitir todos los dominios

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'], // Define las rutas que deben aceptar CORS
     'allowed_methods' => ['*'], // Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
-    'allowed_origins' => ['https://juandiegows.com'], // Permite específicamente este origen
+    'allowed_origins' => ['*'], // Permite específicamente este origen
     'allowed_origins_patterns' => [], // Si necesitas usar patrones (regex)
     'allowed_headers' => ['*'], // Permite todos los encabezados
     'exposed_headers' => [],
