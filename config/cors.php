dabf8c68c2b45edb6e9849396d3cc11faa5cad3a
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-01-21 23:15:49 -0500

    cambiar cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'], // Permite todas las rutas, ya que no tienes protección
     'allowed_methods' => ['*'], // Permite todos los métodos HTTP
-    'allowed_origins' => ['http://localhost:4200'], // Especifica el origen del frontend
+    'allowed_origins' => ['*'], // Especifica el origen del frontend
     'allowed_origins_patterns' => [], // Vacío si no usas patrones dinámicos
     'allowed_headers' => ['*'], // Permite todas las cabeceras
     'exposed_headers' => [], // Define si necesitas exponer encabezados específicos
